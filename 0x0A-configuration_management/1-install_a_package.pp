1a7122f9d665d7a166fb80dd2be62ce5aee788a5
root <root@DESKTOP-N65KJQ1>
2024-01-19 23:11:56 +0300

    soln to 1

---
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -0,0 +1,9 @@
+# installs the package puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}# installs the package puppet-lint
+package { 'puppet-lint':
+  ensure   => '2.1.1',
+  provider => 'gem',
+}
